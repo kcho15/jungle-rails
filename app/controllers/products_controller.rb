5b79a89e8b638af1b02210a24ffc60c61b3932d4
Kevin Cho <kevcho604@gmail.com>
2023-05-03 14:13:23 -0700

    Fix bug for price formating not showing $ sign

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order(created_at: :desc)
   end
-
+ 
   def show
     @product = Product.find params[:id]
   end
