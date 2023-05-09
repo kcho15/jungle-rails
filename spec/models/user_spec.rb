51f66e976b3c440af5589cf5a724b128f7476659
Kevin Cho <kevcho604@gmail.com>
2023-05-08 20:28:40 -0700

    Add validations to user_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    before(:each) do
+      @user = User.new do |testUser|
+        testUser.first_name = 'Tester'
+        testUser.last_name = 'McTesterson'
+        testUser.email = 'tester@gmail.com'
+        testUser.password = 'password'
+        testUser.password_confirmation = 'password'
+      end
+    end
