15485db0635908d0216c994dfb79a36f38c942e8
Kevin Cho <kevcho604@gmail.com>
2023-05-09 13:57:45 -0700

    Add test for password min length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
-class User < ActiveRecord::Base
-  
+class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, 
+    presence: true,
+    uniqueness: { case_sensitive: false }
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase 
+    user = User.find_by_email(email)
+    user_password = user.authenticate(password) 
+    user && user_password ? user : nil
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    before(:each) do
-      @user = User.new do |testUser|
-        testUser.first_name = 'Tester'
-        testUser.last_name = 'McTesterson'
-        testUser.email = 'tester@gmail.com'
-        testUser.password = 'password'
-        testUser.password_confirmation = 'password'
-      end
+    before do
+      @user = User.new(email: 'test@test.com', first_name: 'Tester', last_name: 'McTesterson', password: "password", password_confirmation: "password")
+      @user.save
     end
-
     
     it 'is not valid without a password' do
       @user.password = nil
@@ -29,18 +23,24 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
 
+    it 'validates that emails are not case sensitive' do
+      @user1 = User.new(email: 'TEST@TEST.COM', first_name: 'Tester', last_name: 'Tester', password: "password", password_confirmation: "password")
+      expect(@user1).to_not be_valid
+      expect(@user1.errors.full_messages).to include("Email has already been taken")
+    end
 
-  end
-
+    it 'validates that the password must at least six characters' do
+      @user.password = "1234"
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+  
   describe '.authenticate_with_credentials' do
-    before(:each) do
-      @user = User.new do |testUser|
-        testUser.first_name = 'Tester'
-        testUser.last_name = 'McTesterson'
-        testUser.email = 'tester@gmail.com'
-        testUser.password = 'password'
-        testUser.password_confirmation = 'password'
-      end
+    before do
+      @user = User.new(email: 'test@test.com', first_name: 'Tester', last_name: 'McTesterson', password: "password", password_confirmation: "password")
+      @user.save
     end
   end
+end
 end
