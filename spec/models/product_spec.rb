1a929fa0c3c0ffc745f38dffee5b3ce4947109b3
Kevin Cho <kevcho604@gmail.com>
2023-05-08 20:13:55 -0700

    Add validation tests for product checking name, price, quantity, and category all present

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,40 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   
   describe 'Validations' do
-  # validation tests/examples here
+    before do
+      @category = Category.create(name: "Test Category")
+      @product = Product.new(name: "Test Product", price_cents: 9999, quantity: 99, category: @category)
+    end
 
-  end
+      it 'is valid with all four fields' do
+        expect(@product).to be_valid
+      end
+
+      it 'is not valid without a name' do
+        @product.name = nil
+        @product.save
+        expect(@product).to_not be_valid
+      end
+      
+      it 'is not valid without a price' do
+        @product.price_cents = nil
+        @product.save
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Price cannot be blank")
+      end
+
+      it 'is not valid without a quantity' do
+        @product.quantity = nil
+        @product.save
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Quantity cannot be blank")
+      end
+
+      it 'is not valid without a category' do
+        @product.category = nil
+        @product.save
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Category cannot be blank")
+      end
+    end
 end
