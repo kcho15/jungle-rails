7d2d42e2bd2fd5473dcad0dcd8715f958511cb2a
Kevin Cho <kevcho604@gmail.com>
2023-05-09 00:52:33 -0700

    Add user_spec tests for password, and all fields filled

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,36 @@ RSpec.describe User, type: :model do
         testUser.password_confirmation = 'password'
       end
     end
+
+    
+    it 'is not valid without a password' do
+      @user.password = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    
+    it "is not valid when password and password_confirmation don't match" do
+      @user.password = 'password'
+      @user.password_confirmation = 'passwords'
+      expect(@user).to_not be_valid
+    end
+    
+    it 'validates that all fields are filled' do
+      expect(@user).to be_valid
+    end
+
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    before(:each) do
+      @user = User.new do |testUser|
+        testUser.first_name = 'Tester'
+        testUser.last_name = 'McTesterson'
+        testUser.email = 'tester@gmail.com'
+        testUser.password = 'password'
+        testUser.password_confirmation = 'password'
+      end
+    end
   end
 end
