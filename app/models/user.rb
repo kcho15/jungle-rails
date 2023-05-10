abcd2448d276105c18f0eb8be3933ab0debdfec7
Kevin Cho <kevcho604@gmail.com>
2023-05-09 17:17:05 -0700

    Fix user authentication bug on sign up to login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, 
-    presence: true,
-    uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password,
     presence: true,
     confirmation: true,
@@ -13,10 +11,12 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, password)
-    email = email.strip.downcase 
-    user = User.find_by_email(email)
-    user_password = user.authenticate(password) 
-    user && user_password ? user : nil
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
   end
 
 end
